0a9b7b2ec139d646980736c794d857ccd3465ab3
Andreas Kissner <andikiss500@gmail.com>
2026-01-07 16:04:49 +0100

    feat: Change Hero-header in Header / Put a new component in shared = primaer-btn

---
+++ src/app/shared/primaer-btn/primaer-btn.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primaer-btn',
+  imports: [],
+  templateUrl: './primaer-btn.component.html',
+  styleUrl: './primaer-btn.component.scss',
+})
+export class PrimaerBtnComponent {
+
+}
