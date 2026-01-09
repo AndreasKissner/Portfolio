768dd17d993511bd80d0a929812026a42b643dd0
Andreas Kissner <andikiss500@gmail.com>
2026-01-09 19:30:16 +0100

    feat: All components from hero - contact base ist make

--- src/app/allServices/scroll-observer.service.ts
+++ src/app/allServices/scroll-observer.service.ts
@@ -12,7 +12,7 @@ export class ScrollObserverService {
   init(sectionIds: string[]) {
     const options = {
       root: null,
-      threshold: 0.6 
+      threshold: 0.6
     };
 
     const observer = new IntersectionObserver((entries) => {
